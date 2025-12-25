c2a54c18a06f006680b33a6083e77d5fc14181b8
Mohamed Asif <mohamedasif092006@gmail.com>
2025-12-25 12:31:15 +0530

    Time: 0 ms (100%), Space: 43.6 MB (15.29%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int in = 0;
+        for(int i =0;i < nums.length;i++){
+            if(val != nums[i]){
+                nums[in] = nums[i];
+                in++;
+            }
+        }
+        return in;
+    }
+}
