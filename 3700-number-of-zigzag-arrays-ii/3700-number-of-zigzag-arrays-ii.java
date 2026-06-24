222e5bff37d0bb8f72e67606fdf03ca461992bef
Mohamed Asif <mohamedasif092006@gmail.com>
2026-06-24 20:21:34 +0530

    Time: 81 ms (100%), Space: 46.6 MB (80.65%) - LeetHub

--- 3700-number-of-zigzag-arrays-ii/3700-number-of-zigzag-arrays-ii.java
+++ 3700-number-of-zigzag-arrays-ii/3700-number-of-zigzag-arrays-ii.java
@@ -1,73 +1,75 @@
 class Solution {
-    private static final long MOD = 1_000_000_007L;
+    public int zigZagArrays(int n, int l, int r) {
+        int len = r - l + 1;
+        long[][] m1 = new long[len][len];
+        long[][] m2 = new long[len][len];
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                m1[i][j] = 1;
+            }
+            for (int j = 0; j < i; j++) {
+                m2[i][j] = 1;
+            }
+        }
+        long[][] m = pro(m1, m2);
+        long[] arr = new long[len];
+        Arrays.fill(arr, 1);
+        n--;
+        int count = n / 2;
+        while (count > 0) {
+            if (count % 2 == 1)
+                arr = pro(arr, m);
+            m = pro(m);
+            count /= 2;
+        }
+        if (n % 2 == 1)
+            arr = pro(arr, m1);
+        long res = 0;
+        for (long num : arr) {
+            res += num;
+        }
+        return (int) (res * 2 % mod);
+    }
 
-    private long[][] multiply(long[][] A, long[][] B) {
-        int sz = A.length;
+    int mod = 1_000_000_007;
 
-        long[][] C = new long[sz][sz];
-
-        for (int i = 0; i < sz; i++) {
-            for (int k = 0; k < sz; k++) {
-                if (A[i][k] == 0) continue;
-
-                long cur = A[i][k];
-
-                for (int j = 0; j < sz; j++) {
-                    if (B[k][j] == 0) continue;
-
-                    C[i][j] = (C[i][j] + cur * B[k][j]) % MOD;
+    public long[][] pro(long[][] a) {
+        long[][] res = new long[a.length][a[0].length];
+        for (int i = 0; i < res.length; i++) {
+            for (int k = 0; k < res.length; k++) {
+                if (a[i][k] == 0)
+                    continue;
+                for (int j = 0; j < res.length; j++) {
+                    res[i][j] = (res[i][j] + a[i][k] * a[k][j]) % mod;
                 }
             }
         }
-
-        return C;
+        return res;
     }
 
-    public int zigZagArrays(int n, int l, int r) {
-        int m = r - l + 1;
-        int sz = 2 * m;
-
-        long[][] T = new long[sz][sz];
-
-        for (int x = 0; x < m; x++) {
-
-            for (int y = x + 1; y < m; y++) {
-                T[x][m + y] = 1;
-            }
-
-            for (int y = 0; y < x; y++) {
-                T[m + x][y] = 1;
+    public long[][] pro(long[][] a, long[][] b) {
+        long[][] res = new long[a.length][a[0].length];
+        for (int i = 0; i < res.length; i++) {
+            for (int k = 0; k < res.length; k++) {
+                if (a[i][k] == 0)
+                    continue;
+                for (int j = 0; j < res.length; j++) {
+                    res[i][j] = (res[i][j] + a[i][k] * b[k][j]) % mod;
+                }
             }
         }
+        return res;
+    }
 
-        long[][] result = new long[sz][sz];
-        for (int i = 0; i < sz; i++) {
-            result[i][i] = 1;
-        }
-
-        long power = n - 1;
-
-        while (power > 0) {
-            if ((power & 1) == 1) {
-                result = multiply(result, T);
+    public long[] pro(long[] a, long[][] b) {
+        long[] res = new long[a.length];
+        for (int j = 0; j < res.length; j++) {
+            if (a[j] == 0)
+                continue;
+            for (int i = 0; i < res.length; i++) {
+                res[i] = (res[i] + a[j] * b[j][i]) % mod;
             }
-
-            T = multiply(T, T);
-            power >>= 1;
         }
-
-        long answer = 0;
-
-        for (int i = 0; i < sz; i++) {
-            long rowSum = 0;
-
-            for (int j = 0; j < sz; j++) {
-                rowSum = (rowSum + result[i][j]) % MOD;
-            }
-
-            answer = (answer + rowSum) % MOD;
-        }
-
-        return (int) answer;
+        return res;
     }
 }
